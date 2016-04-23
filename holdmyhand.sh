8348725ccd8078d1a1a8eb6e6c130e57bc35e06c
Turbine1991 <turbine1991@gmail.com>
2016-04-23 15:18:07 +1200

    Delete holdmyhand.sh

--- holdmyhand.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#GitHub This: https://github.com/Turbine1991/build_ubuntu_kernel_wastedcores
-#GitHub WastedCores: https://github.com/jplozi/wastedcores
-
-sudo bash clone
-sudo bash menu
-sudo bash build
